d7cc3839b359985aadf9cccbdbaf8a8bbf81e341
Julian Brasseur <brasseurjulian2@gmail.com>
2023-11-07 11:47:08 +0100

    Création Inscritpion

---
+++ MakiManga/src/views/RegisterView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="register-container">
+    <h2>Inscription</h2>
+    <p>(Les champs marqués avec * sont obligatoires)</p>
+
+    <label for="firstNameInput">Prénom : *</label>
+    <input type="text" name="firstName" id="firstNameInput">
+
+    <label for="lastNameInput">Prénom : *</label>
+    <input type="text" name="lastName" id="lastNameInput">
+
+    <label for="emailInput">Prénom : *</label>
+    <input type="email" name="email" id="emailInput">
+
+    <label for="passwordInput">Prénom : *</label>
+    <input type="password" name="password" id="passwordInput">
+
+    <label for="passwordConfirmInput">Prénom : *</label>
+    <input type="password" name="passwordConfirm" id="passwordConfirmInput">
+
+    <label for="telInput">Prénom : *</label>
+    <input type="tel" name="phoneNumber" id="telInput">
+
+    <label for="termsConditions">J'ai lu et j'accepte les termes et conditions</label>
+    <input type="checkbox" name="termsAgree" id="termsConditions">
+  </div>
+</template>
