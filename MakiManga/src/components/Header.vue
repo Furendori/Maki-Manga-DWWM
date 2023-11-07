bd13e7436d009c474a886c34de40aa2194260fa5
Your NameAngelique-D <angelique.didillon@gmail.com>
2023-11-07 11:44:06 +0100

    ✅ 🔥  Add header component

---
+++ MakiManga/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <img src="../assets/img/LOGO_MAKI_MANGA.png" alt="">
+    </header>
+</template>
+
+<style scoped lang="scss">
+
+ header {
+    height: 140px;
+    background-color: #1C2942;
+ }
+</style>
