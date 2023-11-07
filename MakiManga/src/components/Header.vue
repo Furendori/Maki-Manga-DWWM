fd63dbf4a9e104f72d8646226de28f61fc08f06e
Your NameAngelique-D <angelique.didillon@gmail.com>
2023-11-07 11:53:28 +0100

    ✅ Add Routerlinkto header

--- MakiManga/src/components/Header.vue
+++ MakiManga/src/components/Header.vue
@@ -1,6 +1,11 @@
 <template>
     <header>
-        <img src="../assets/img/LOGO_MAKI_MANGA.png" alt="">
+        <router-link to="/"><img src="../assets/img/LOGO_MAKI_MANGA.png" alt=""></router-link>
+        <nav>
+            <router-link to="#">Licences</router-link>
+            <router-link to="#">Produits</router-link>
+            <router-link to="#">Contact</router-link>
+        </nav>
     </header>
 </template>
 
@@ -9,5 +14,19 @@
  header {
     height: 140px;
     background-color: #1C2942;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    img {
+        height: 150px;
+        width: 150px;
+    }
+
+    nav {
+        a {
+            color: white;
+        }
+    }
  }
 </style>
