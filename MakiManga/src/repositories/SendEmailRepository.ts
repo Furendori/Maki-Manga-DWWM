8472e859c7d0c825401fa142f56f148abec4f80e
Alex <126157943+Txyron@users.noreply.github.com>
2023-11-22 09:16:53 -0800

    Pull dev by Alex

--- MakiManga/src/repositories/SendEmailRepository.ts
+++
@@ -1,8 +0,0 @@
-import { BaseRepository } from "./BaseRepository"
-
-export class SendEmailRepository extends BaseRepository {
-    
-    createUser = async (body?: {}) => {
-        return await this.request(`/users`, 'POST', body);
-    }
-}
